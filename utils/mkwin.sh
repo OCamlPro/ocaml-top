878c24b150dc8d60967ae2c2b7d7cac40fdf1a4b
Louis Gesbert <louis.gesbert@ocamlpro.com>
2013-06-27 17:36:33 +0200

    Fixes to the windows setup helper script

--- utils/mkwin.sh
+++ utils/mkwin.sh
@@ -4,8 +4,10 @@ PREFIX=/cygdrive/c/usr
 
 echo "[41mDOWNLOAD[m"
 
-# cygwin
-wget http://cygwin.com/setup.exe
+# cygwin (to do by hand)
+# required packages: make mingw64-i686-gcc-core wget zip unzip pkg-config
+# recommended: openssh git emacs tig...
+# wget http://cygwin.com/setup.exe
 
 # iconv
 wget http://sourceforge.net/projects/gnuwin32/files/libiconv/1.9.2-1/libiconv-1.9.2-1-lib.zip
@@ -19,8 +21,8 @@ wget ftp://ftp.gnome.org/pub/gnome/binaries/win32/gtk+/2.24/gtk+-bundle_2.24.10-
 wget http://ftp.acc.umu.se/pub/gnome/binaries/win32/gtksourceview/2.10/gtksourceview-dev-2.10.0.zip
 wget http://ftp.acc.umu.se/pub/gnome/binaries/win32/gtksourceview/2.10/gtksourceview-2.10.0.zip
 
-# ocaml
-wget http://gallium.inria.fr/~protzenk/caml-installer/ocaml-4.00.1-i686-mingw64.exe
+# ocaml (to do by hand, recommended: minimum installation)
+#wget http://gallium.inria.fr/~protzenk/caml-installer/ocaml-4.00.1-i686-mingw64.exe
 
 # lablgtk
 wget http://forge.ocamlcore.org/frs/download.php/979/lablgtk-2.16.0.tar.gz
@@ -32,18 +34,36 @@ echo "[41mUNZIP[m"
 DL=$PWD
 mkdir -p "$PREFIX"
 cd "$PREFIX"
-unzip "$DL"/gtk+-bundle_2*.zip
-unzip "$DL"/gtksourceview-2*.zip
-unzip "$DL"/gtksourceview-dev-2*.zip
-unzip "$DL"/libiconv-*-bin.zip
+unzip -n "$DL"/gtk+-bundle_2*.zip
+unzip -n "$DL"/gtksourceview-2*.zip
+unzip -n "$DL"/gtksourceview-dev-2*.zip
+unzip -n "$DL"/libiconv-*-bin.zip
 mv bin/libiconv2.dll bin/iconv.dll
-unzip "$DL"/libiconv-*-lib.zip
+unzip -n "$DL"/libiconv-*-lib.zip
+
 libxml=$(ls "$DL"/libxml2-*.zip)
-unzip "$libxml"
+unzip -n "$libxml"
 libxmldir=$(basename "$libxml" .zip)
 cp -r "$libxmldir"/* .
 rm -rf "$libxmldir"
 mv bin/libxml2.dll bin/libxml2-2.dll
+cat >lib/pkgconfig/libxml-2.0.pc <<EOF
+prefix=$PREFIX
+exec_prefix=\${prefix}
+libdir=\${prefix}/lib
+includedir=\${prefix}/include
+modules=1
+
+Name: libXML
+Version: 2.7.0
+Description: libXML library version2.
+Requires:
+Libs: -L\${libdir} -lxml2
+Libs.private:  -lz  -lm
+Cflags: -I\${includedir}/libxml2
+EOF
+
+find . -iname \*.dll -exec chmod a+x {} \;
 
 sed -i 's#^prefix=.*#prefix='"$PREFIX"'#' lib/pkgconfig/*.pc
 export PKG_CONFIG_PATH="$PREFIX"/lib/pkgconfig
@@ -56,7 +76,7 @@ cd $DL
 labl=$(ls lablgtk-*.tar.gz)
 tar -xzf "$labl"
 cd "${labl%.tar.gz}"
-./configure --with-gtksourceview2 --without-glade --without-gnomecanvas
+./configure --with-gtksourceview2 --without-glade --without-gnomecanvas --disable-gtktest
 make
 make opt
 make install
