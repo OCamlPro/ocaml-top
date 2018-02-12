c8a9fa124838dd20fb4cb30bdb20069dd1e3698a
Vasile Rotaru <vrotaru.md@gmail.com>
2018-02-12 22:20:11 +0200

    Removing unused function

--- src/tools.mli
+++ src/tools.mli
@@ -22,10 +22,6 @@ exception Recoverable_error of string
 
 val recover_error: ('a, unit, string, 'b) format4 -> 'a
 
-(** [string_split_chars chars str] cuts [str] at all occurence of any char
-    belonging to [chars]. Empty strings are discarded. *)
-val string_split_chars: string -> string -> string list
-
 (** Splits a string into lines. Newline characters are discarded. Windows
     compatible. If the string ends with a newline, the result will end with
     an empty string (so that no information is lost) *)
