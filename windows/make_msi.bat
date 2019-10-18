196cec20dd3086530bd767ff22e7f1d29e45c09a
Louis Gesbert <louis.gesbert@ocamlpro.com>
2019-10-18 17:52:05 +0200

    Fix .bat syntax
    ¯\_(ツ)_/¯

--- windows/make_msi.bat
+++ windows/make_msi.bat
@@ -1,8 +1,8 @@
 PATH=$PATH;"C:\Program Files (x86)\WiX Toolset v3.10\bin"
 
-for %i in (data bin doc lib share) do heat.exe dir ocaml-top/%i -srd -dr %i -cg %i -gg -var var.%iDir -out %i.wxs
+for %%i in (data bin doc lib share) do heat.exe dir ocaml-top/%%i -srd -dr %%i -cg %%i -gg -var var.%%iDir -out %%i.wxs
 
-for %i in (data bin doc lib share) do candle.exe -d"%iDir=ocaml-top\\%i" -out %i.wixobj %i.wxs
+for %%i in (data bin doc lib share) do candle.exe -d"%%iDir=ocaml-top\\%%i" -out %%i.wixobj %%i.wxs
 
 candle.exe -out ocaml-top.wixobj ocaml-top.wxs
 
